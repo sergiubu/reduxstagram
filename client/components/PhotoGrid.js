df320352443dbf98df8e801f604d2ab18a28f583
Sergiu <sil3ent@yahoo.com>
2019-04-07 14:21:59 +0300

    Create components & implement Router

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PhotoGrid = React.createClass({
+  render() {
+    return <div className="photo-grid">I'm the PhotoGrid</div>;
+  }
+});
+
+export default PhotoGrid;
